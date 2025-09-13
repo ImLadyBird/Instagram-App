3bfb85487556992bda9177ee860db80fc479f21a
Milad Bajelan <iammiladbajelan@gmail.com>
2025-09-13 18:14:49 +0330

    fix

--- src/Component/Pages/Aside.jsx
+++ src/Component/Pages/Aside.jsx
@@ -3,6 +3,7 @@ import Home from "../../images/Home.svg"
 import Search from "../../images/Search.svg"
 import Create from "../../images/Create.svg"
 import Profile from "../../images/Profile.svg"
+import { Link, NavLink } from "react-router";
 
 
 
@@ -15,10 +16,10 @@ export default function Aside() {
       <div>
         <img src={insta} alt="" className="max-h-[35px] mb-4" />
         <div className="flex gap-4 flex-col">
-        <p className="flex flex-row gap-2" ><img src={Home} alt="Home-Logo" /> Home</p>
-        <p className="flex flex-row gap-2" ><img src={Search} alt="Search-Logo" />Search</p>
-        <p className="flex flex-row gap-2" ><img src={Create} alt="Create-Logo" />Create</p>
-        <p className="flex flex-row gap-2" ><img src={Profile} alt="Profile-Logo" />Profile</p>
+        <NavLink to={"/login/postcard"} className="flex flex-row gap-2" ><img src={Home} alt="Home-Logo" /> Home</NavLink>
+        <NavLink to={"/login"} className="flex flex-row gap-2" ><img src={Search} alt="Search-Logo" />Search</NavLink>
+        <NavLink to={"/login/postcard"} className="flex flex-row gap-2" ><img src={Create} alt="Create-Logo" />Create</NavLink>
+        <NavLink to={"/login/postcard"} className="flex flex-row gap-2" ><img src={Profile} alt="Profile-Logo" />Profile</NavLink>
         </div>
         
       </div>
