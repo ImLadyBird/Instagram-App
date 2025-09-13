1e115a092b1d8df69c3d09393f1f38c19adaebe5
Negin Momeni <neginmomeni214@gmail.com>
2025-09-13 15:53:59 +0330

    feat : add login api

--- src/Component/Pages/Login.jsx
+++ src/Component/Pages/Login.jsx
@@ -1,34 +1,92 @@
+import { useForm } from "react-hook-form";
 import insta from "../../images/instamain.svg";
-import picInsta from "../../images/picInsta.svg"
+import picInsta from "../../images/picInsta.svg";
 import { Link } from "react-router";
+import { client } from "../../Lib/idex";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
 
-
+const forSchema = z.object({
+  username: z
+    .string()
+    .min(3, { message: "must be atleast 3 characters" })
+    .max(20, { message: "must be atmost 20 characters" }),
+  password: z
+    .string()
+    .min(8, { message: "must be atleast 8 characters" })
+    .max(20, { message: "must be atmost 20 characters" }),
+});
 
 export default function Login() {
-    return (
+  const [showPassword, setShowPassword] = useState(false);
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(forSchema),
+  });
 
-        <div className="flex min-h-screen items-center justify-center gap-8">
-            <div>
-                <img src={picInsta} alt="pic" className="max-h-[666px]" />
-            </div>
-        <div className="flex flex-col border-1 border-[#9b9191b0] p-[50px] gap-2 ">
-          <img src={insta} alt="Logo" className="max-w-320px" />
+  async function loginFunc({ username, password }) {
+    try {
+      const response = await client.post("/user/login", { username, password });
+      localStorage.setItem("token", response.data.jwt);
+      console.log(response);
+    } catch {
+      console.log("error");
+    }
+  }
 
-         <form className="flex flex-col gap-[18px]">
-         <input className="bg-gray-100 px-8 py-4 rounded-[8px] border-1 border-1[8a8888]" type="text" placeholder="User Name" />
-         <input className="bg-gray-100 px-8 py-4 rounded-[8px] border-1 border-1[8a8888]" type="password" placeholder="Password" />
-         <button type="submit" className="bg-[#44B8FA] text-white font-bold py-3 rounded-[12px] border-1 border-[#8a8888a9]">Log in</button> 
-         </form>
-         <div className="flex flex-row gap-1 justify-center">
+  return (
+    <div className="flex min-h-screen items-center justify-center gap-8">
+      <div>
+        <img src={picInsta} alt="pic" className="max-h-[666px]" />
+      </div>
+      <div className="flex flex-col border-1 border-[#9b9191b0] p-[50px] gap-2 ">
+        <img src={insta} alt="Logo" className="max-w-320px" />
+
+        <form
+          className="flex flex-col gap-[18px]"
+          onSubmit={handleSubmit(loginFunc)}
+        >
+          <input
+            {...register("username")}
+            className="bg-gray-100 px-8 py-4 rounded-[8px] border-1 border-1[8a8888]"
+            type="text"
+            placeholder="User Name"
+          />
+          <span className="text-red-500">{errors?.username?.message}</span>
+          <div className="bg-gray-100 px-8 py-4 rounded-[8px] border-1 border-1[8a8888] flex gap-15">
+            <input
+            className="border-none bg-transparent outline-none"
+              {...register("password")}
+              placeholder="Password"
+              type={showPassword ? "text" : "password"}
+            />
+            <button
+              type="button"
+              onClick={() => setShowPassword(!showPassword)}
+              value="show"
+            >
+              show
+            </button>
+          </div>
+          <span className="text-red-500">{errors?.password?.message}</span>
+          <button
+            type="submit"
+            className="bg-[#44B8FA] text-white font-bold py-3 rounded-[12px] border-1 border-[#8a8888a9]"
+          >
+            Log in
+          </button>
+        </form>
+        <div className="flex flex-row gap-1 justify-center">
           <p>Don’t have an account?</p>
-         <Link to={"/login"} className="text-[#44B8FA]">
+          <Link to={"/login"} className="text-[#44B8FA]">
             Login
           </Link>
         </div>
-         </div>
-        </div>
-        
-           
-        
-    )
+      </div>
+    </div>
+  );
 }
--- src/Component/Pages/SignUp.jsx
+++ src/Component/Pages/SignUp.jsx
@@ -3,7 +3,8 @@ import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { client } from "../../Lib/idex";
-import { useNavigate , Link } from "react-router";
+import { useNavigate, Link } from "react-router";
+import { useState } from "react";
 
 const forSchema = z.object({
   username: z
@@ -18,6 +19,7 @@ const forSchema = z.object({
 });
 
 export default function SignUp() {
+    const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate();
 
   const {
@@ -30,14 +32,11 @@ export default function SignUp() {
 
   async function subForm(data) {
     try {
-
-      const response = await client.post("/user/signup",data)
-      navigate("/login")
+      const response = await client.post("/user/signup", data);
+      navigate("/login");
       console.log(response.data);
-      
     } catch {
       console.log("error");
-      
     }
   }
 
@@ -64,12 +63,21 @@ export default function SignUp() {
             className="bg-gray-100 px-8 py-4 rounded-[8px] border-1 border-[#8a8888]"
           />
           <span className="text-red-500">{errors?.username?.message}</span>
-          <input
-            {...register("password")}
-            type="password"
-            placeholder="Password"
-            className="bg-gray-100 px-8 py-4 rounded-[8px] border-1 border-[#8a8888]"
-          />
+          <div className="bg-gray-100 px-8 py-4 rounded-[8px] border-1 border-1[8a8888] flex gap-15">
+            <input
+              className="border-none bg-transparent outline-none"
+              {...register("password")}
+              placeholder="Password"
+              type={showPassword ? "text" : "password"}
+            />
+            <button
+              type="button"
+              onClick={() => setShowPassword(!showPassword)}
+              value="show"
+            >
+              show
+            </button>
+          </div>
           <span className="text-red-500">{errors?.password?.message}</span>
           <button
             type="submit"
