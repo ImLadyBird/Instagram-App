fba9fff3560be98a2f47c715dd63d692b2f0505a
ZwX <akbarzadedhzox@gmail.com>
2025-09-14 10:32:27 +0330

    add component page

--- src/route.jsx
+++ src/route.jsx
@@ -4,27 +4,32 @@ import Login from "./Component/Pages/Login";
 import Profile from "./Component/Pages/Profile";
 import Postcard from "./Component/Pages/Postcard";
 import Search from "./Component/Pages/Search";
+import UserPage from "./Component/UserPage";
 
 export const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <SignUp/>
-    },
-    {
-        path: "/login",
-        element: <Login/>
-    },
-    {
-        path : "/login/postcard",
-        element : <Postcard/>
-    },
-     {
+  {
+    path: "/",
+    element: <SignUp />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/login/postcard",
+    element: <Postcard />,
+  },
+  {
     path: "/login/postcard/profile",
     element: <Profile />,
   },
-    {
-        path : "/login/postcard/search",   
-        element : <Search/>
-    }
-])
-  
+  {
+    path: "/login/postcard/search",
+    element: <Search />,
+  },
+  {
+    path: "/login/postcard/userpage",
+    element: <UserPage/>
+  }
+
+]);
